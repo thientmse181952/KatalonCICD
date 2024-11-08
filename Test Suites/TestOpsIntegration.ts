1a9df4e55f604c540e759655c059c6ee95675e11
thientmse181952 <thientmse181952@fpt.edu.vn>
2024-11-08 19:35:14 +0700

    add new test case and test suite for testops

---
+++ Test Suites/TestOpsIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestOpsIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>49219f2a-d33d-4064-b065-48e84b0b4f5a</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4d0d041-f86d-488d-ad9e-537a44761033</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOpsIntegration/TestOpsTesting</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
