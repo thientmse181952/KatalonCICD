4320dfde4b82f51a14c7ab20066e4aa5029c4a2d
thientmse181952 <thientmse181952@fpt.edu.vn>
2024-11-09 10:26:26 +0700

    update test case

--- Scripts/TestOpsIntegration/TestOpsTesting/Script1731068805402.groovy
+++ Scripts/TestOpsIntegration/TestOpsTesting/Script1731068805402.groovy
@@ -40,7 +40,7 @@ WebUI.click(findTestObject('Object Repository/TestOpsIntegration/Page_CURA Healt
 WebUI.click(findTestObject('Object Repository/TestOpsIntegration/Page_CURA Healthcare Service/td_21'))
 
 WebUI.setText(findTestObject('Object Repository/TestOpsIntegration/Page_CURA Healthcare Service/textarea_Comment_comment'), 
-    'Appointment Information')
+    'Appointment information')
 
 WebUI.click(findTestObject('Object Repository/TestOpsIntegration/Page_CURA Healthcare Service/button_Book Appointment'))
 
